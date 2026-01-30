7451c4ff4779d1c5be81798f2ab1dcc57d8289f4
jorgegabrielti <jorgegabriel.ti@gmail.com>
2026-01-29 22:01:16 -0300

    feat: adiciona exercício 1.1 - exibir nome do comando
    - Modifica programa echo para incluir os.Args[0]
    - Usa strings.Join com slice completo (os.Args[0:])
    - Demonstra diferença entre os.Args[0:] e os.Args[1:]

---
+++ chapter_1-tutorial/1.2_ argumentos_de_linha_de_comando/1.2_argumentos_de_linha_de_comandos_exercicios/echo_exe1.go
@@ -0,0 +1,12 @@
+// Exercicio 1.1: Modifique o programa echo para exibir também  os.Args[0], que é o nome do comando que o chamou.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Join(os.Args[0:], " "))
+}
